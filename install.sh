83ad2837e0c7f888168e4c423526be19447b5612
dawilco <ki4yvc@gmail.com>
2019-11-18 09:49:54 -0500

    puts up apache2 and php ini

--- install.sh
+++ install.sh
@@ -23,3 +23,23 @@ sudo ufw allow in "Apache Full"
 
 # Allow Read/Write for Owner
 sudo chmod -R 0755 /var/www/html/
+
+# Copy new Apache file
+# Hides version an os id
+# Disables directory listing
+
+mv apache2.conf /etc/apache2
+
+# apply changes to apache
+sudo systemctl start apache2
+
+# copy new php file
+# disables remote code execution
+# limit php to only access /var/www files
+mv php.ini /etc/php/7.0/apache2
+
+# write protect all the config files so an attacker canoot change them
+chattr +i /etc/php.ini
+chattr +i /etc/php.d/*
+chattr +i /etc/my.cnf
+chattr +i /etc/httpd/conf/httpd.conf
