74b2bc7d243c5694ac8ce170c06ee8e9c546f542
dawilco <ki4yvc@gmail.com>
2019-11-18 11:52:05 -0500

    add fail to ban

--- install.sh
+++ install.sh
@@ -3,6 +3,19 @@
 # Update Package Index
 sudo apt update
 
+# autmotically keep our machine up to date
+sudo apt-get install unattended-upgrades
+
+# install fail to ban
+sudo apt-get install fail2ban
+
+# configuratoin of fail to ban
+sudo mv jail.local /etc/fail2ban
+
+# apply confif and allow to start on boot
+sudo systemctl restart fail2ban.service
+sudo systemctl enable fail2ban.service
+
 # Install Apache2, MySQL, PHP
 sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli
 
@@ -28,7 +41,7 @@ sudo chmod -R 0755 /var/www/html/
 # Hides version an os id
 # Disables directory listing
 
-mv apache2.conf /etc/apache2
+sudo mv apache2.conf /etc/apache2
 
 # apply changes to apache
 sudo systemctl start apache2
@@ -36,10 +49,16 @@ sudo systemctl start apache2
 # copy new php file
 # disables remote code execution
 # limit php to only access /var/www files
-mv php.ini /etc/php/7.0/apache2
+sudo mv php.ini /etc/php/7.0/apache2
+
+# Mod_security is a web application firewall (WAF) which can be installed as an additional module for Apache. It can be used to protect the web server from numerous attacks like SQL injections, session hijacking, cross site scripting, bad user agents and many others
+sudo apt-get install libapache2-modsecurity
+sudo a2enmod security2
+sudo mv modsecurity.conf /etc/modsecurity/modsecurity.conf
 
 # write protect all the config files so an attacker canoot change them
 chattr +i /etc/php.ini
 chattr +i /etc/php.d/*
 chattr +i /etc/my.cnf
 chattr +i /etc/httpd/conf/httpd.conf
+chattr +i /etc/fail2ban/jail.local
